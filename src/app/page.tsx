c5dd364f128b13b61dd3e3136e19661c8f73dadc
diegoandres22 <diego.a.v3005@gmail.com>
2025-06-07 21:27:36 -0400

    FIX: slint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client"
 import { Start } from "@/components/1-screens";
-import { Spinner } from "@heroui/react";
 
 export default function Home() {
   return (
     <div className="flex gap-4 w-screen h-screen justify-evenly items-center ">
-      {/* <Spinner color="white" label="white" labelColor="foreground" />
-      <Spinner color="primary" label="Primary" labelColor="primary" />
-      <Spinner color="secondary" label="Secondary" labelColor="secondary" />
-      <Spinner color="success" label="Success" labelColor="success" />
-      <Spinner color="warning" label="Warning" labelColor="warning" />
-      <Spinner color="danger" label="Danger" labelColor="danger" /> */}
       <Start></Start>
     </div>
   );
